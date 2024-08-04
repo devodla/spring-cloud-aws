54a098848d1eef0b02075c6a2c7983ab5585e36f
Huillca, Reynaldo <lymreynaldo@icloud.com>
2024-08-03 21:47:43 -0300

    chore: init project

---
+++ src/main/java/uk/layme/cloudsqs/CloudSqsApplication.java
@@ -0,0 +1,13 @@
+package uk.layme.cloudsqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSqsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudSqsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/layme/cloudsqs/CloudSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.layme.cloudsqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudSqsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
